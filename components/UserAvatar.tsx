e846e329aab49701c30a188d9c5e78b504472727
saqib-tf <saqib.razzaq@techfoyer.ai>
2025-06-20 14:51:13 +0500

    same origin cors fix

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -20,7 +20,7 @@ export default function UserAvatar() {
         },
       });
     } else {
-      fetch("/.auth/me")
+      fetch("/.auth/me", { credentials: "same-origin" })
         .then((res) => res.json())
         .then((data: LoggedInUserDto) => {
           setUser(data);
@@ -48,7 +48,7 @@ export default function UserAvatar() {
                 {user ? user.clientPrincipal.userDetails : "No details"}
               </div>
               <button
-                className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-700"
+                className="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-70"
                 onClick={() => {
                   window.location.href = "/.auth/logout";
                   setOpen(false);
